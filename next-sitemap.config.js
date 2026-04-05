a74b84b61ade7f1b75835a0ef469549883f8b491
ehtishamalik <malikehtishamali334@hotmail.com>
2026-04-05 18:13:18 +0500

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://template.ehtishamalik.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "monthly",
+  priority: 0.7,
+  trailingSlash: true,
+};
